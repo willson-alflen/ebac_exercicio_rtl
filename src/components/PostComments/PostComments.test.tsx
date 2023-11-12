1c3d12b618717783c7bfdddd7e5cbf298e9c6603
willson-alflen <willson.alflen@gmail.com>
2023-11-12 10:23:57 -0300

    create PostComments tests

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,37 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from '@testing-library/react'
+// import Post from '.'
+import PostComment from '.'
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+  it('Deve renderizar o componente corretamente', () => {
+    render(<PostComment />)
+    expect(screen.getByText('Comentar')).toBeInTheDocument()
+  })
+  it('Deve existir dois comentários', () => {
+    render(<PostComment />)
+
+    const input = screen.getByTestId('comment-input')
+    const button = screen.getByTestId('submit-button')
+    fireEvent.change(input, { target: { value: 'Primeiro Comentário' } })
+    fireEvent.click(button)
+
+    fireEvent.change(input, { target: { value: 'Segundo Comentário' } })
+    fireEvent.click(button)
+
+    const comments = screen.getAllByTestId('comment-text')
+    expect(comments.length).toBe(2)
+  })
+  it('Deve existir um comentário com valor "Primeiro Comentário"', () => {
+    render(<PostComment />)
+
+    const input = screen.getByTestId('comment-input')
+    const button = screen.getByTestId('submit-button')
+
+    fireEvent.change(input, { target: { value: 'Primeiro Comentário' } })
+    fireEvent.click(button)
+
+    expect(screen.getByTestId('comment-text')).toHaveTextContent(
+      'Primeiro Comentário'
+    )
+  })
+})
--- src/components/PostComments/index.tsx
+++ src/components/PostComments/index.tsx
@@ -1,38 +1,54 @@
-import { FormEvent, useState } from 'react';
-import styles from './PostComments.module.css';
+import { FormEvent, useState } from 'react'
+import styles from './PostComments.module.css'
 
-import Comment from '../../models/Comment';
+import Comment from '../../models/Comment'
 
 const Post = () => {
-    const [comments, setComments] = useState<Comment[]>([]);
-    const [tempComment, setTempComment] = useState('');
+  const [comments, setComments] = useState<Comment[]>([])
+  const [tempComment, setTempComment] = useState('')
 
-    function handleAddComment(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        const newComment = new Comment(comments.length, tempComment);
-        setTempComment('');
-        setComments([...comments, newComment]);
-    }
+  function handleAddComment(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const newComment = new Comment(comments.length, tempComment)
+    setTempComment('')
+    setComments([...comments, newComment])
+  }
 
-    return (
-        <div>
-            <ul className={styles['post-comments']}>
-                {comments.map(({ comment, id }) => (
-                    <li className={styles['post-comment']} key={id}>
-                        <p className={styles['post-comment-content']}>
-                            {comment}
-                        </p>
-                    </li>
-                ))}
-            </ul>
-            <form onSubmit={handleAddComment} className={styles['post-comments-form']}>
-                <textarea value={tempComment} onChange={e => setTempComment(e.target.value)} required className={styles['post-comments-form-textarea']} />
-                <button type="submit" className={styles['post-comments-form-button']}>
-                    Comentar
-                </button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <ul className={styles['post-comments']}>
+        {comments.map(({ comment, id }) => (
+          <li className={styles['post-comment']} key={id}>
+            <p
+              data-testid="comment-text"
+              className={styles['post-comment-content']}
+            >
+              {comment}
+            </p>
+          </li>
+        ))}
+      </ul>
+      <form
+        onSubmit={handleAddComment}
+        className={styles['post-comments-form']}
+      >
+        <textarea
+          data-testid="comment-input"
+          value={tempComment}
+          onChange={(e) => setTempComment(e.target.value)}
+          required
+          className={styles['post-comments-form-textarea']}
+        />
+        <button
+          data-testid="submit-button"
+          type="submit"
+          className={styles['post-comments-form-button']}
+        >
+          Comentar
+        </button>
+      </form>
+    </div>
+  )
 }
 
-export default Post;
+export default Post
